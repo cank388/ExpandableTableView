874a17fe89d19ea37f338e9dc7d9151b41524e75
Can <can@basefy.com>
2019-07-14 17:34:53 +0300

    EmptyViewController

---
+++ ExpandableTableViewExample/EmptyViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EmptyViewController.swift
+//  ExpandableTableViewExample
+//
+//  Created by Can Kalender on 14.07.2019.
+//  Copyright © 2019 Can Kalender. All rights reserved.
+//
+
+import UIKit
+
+class EmptyViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
